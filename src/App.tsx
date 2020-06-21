6817b1c014a6091bd1963dc9fd680a78222c22be
DmitryDor <dmitrydoroshenko80@gmail.com>
2020-06-21 12:06:33 +0300

    Lesson3(props)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import {log} from "util";
+import Accordion from "./components/Accordion/Accordion";
+import Rating from "./components/Rating/Rating";
 
 // function daclaration
 
@@ -10,66 +11,28 @@ function App() {
     console.log("App rendering")
     return (
         <div className="App">
-            <AppTitle/>
-            <Rating/>
-            <Accordion/>
-            <Rating/>
+            <PageTitle title={"This is App component"}/>
+            <PageTitle title={"My friends"}/>
+            Article 1
+            <Rating value={3}/>
+            <Accordion title={"Menu"}/>
+            <Accordion title={"Progress"}/>
+            Article 2
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
         </div>
     );
 }
 
-function AppTitle() {
+function PageTitle(props: any) {
     console.log("Title rendering")
     return (
-        <>This is App component</> //фрагмент
+        <h1>{props.title}</h1>
     )
-}
-
-function Rating() {
-    console.log("Raiting rendering")
-    return (
-        <div>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </div>
-    )
-}
-
-function Accordion() {
-    console.log("Accordion rendering")
-    return (
-        <div>
-            <AccordionTitle />
-            <AccordionBody />
-        </div>
-    )
-
-}
-
-function Star() {
-    console.log("Star rendering")
-    return <div>star</div>
-}
-
-function AccordionTitle() {
-    console.log("AccordionTitle rendering")
-    return (
-        <h2>Menu</h2>
-    )
-}
-function AccordionBody() {
-    console.log("AccordionBody rendering")
-return (
-    <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-    </ul>
-)
 }
 
 export default App;
